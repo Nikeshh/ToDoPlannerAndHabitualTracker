e13c6ea99a4e4ff93c087cbd18c580adcc90afa7
Nikeshh <nikeshhbaskaran01@gmail.com>
2021-04-02 23:42:00 +0530

    Added Login and Registration screens

--- App/trackme/App.js
+++ App/trackme/App.js
@@ -3,6 +3,8 @@ import Launch from './components/Launch';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './redux/Reducers';
+import Login from './components/Login';
+import Register from './components/Register';
 import Settings from './components/Settings';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -13,8 +15,10 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="TrackMe">
-          <Stack.Screen name="TrackMe" component={Launch}></Stack.Screen>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={Login}></Stack.Screen>
+          <Stack.Screen name="Register" component={Register}></Stack.Screen>
+          <Stack.Screen name="Launch" component={Launch}></Stack.Screen>
           <Stack.Screen name="Settings" component={Settings}></Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
